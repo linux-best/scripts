5798a6d34f062e338c06f193071885c4e4884edb
jenkins <amirmahdifhp@gmail.com>
2025-06-07 09:14:14 -0400

    Update ,update.sh

--- update.sh
+++ update.sh
@@ -11,6 +11,6 @@ fi
 if grep -q "Ubuntu" $var || grep -q "Pop" $var
 then
     # update on ubuntu and pop-os
-    sudo apt update 2>/dev/null
+    sudo apt update &>/dev/null
     #sudo apt upgrade
 fi
