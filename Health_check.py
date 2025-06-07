137c52aae1c5d38de719d16e2d16a843ea49e17e
jenkins <amirmahdifhp@gmail.com>
2025-06-07 11:22:35 -0400

    Update ,Health_check.py

--- Health_check.py
+++ Health_check.py
@@ -1,28 +1,28 @@
 import subprocess
 index = 0
-count=0
 
-def a(x,z):
-    if x>z :
-        return 1
+def a(x):
+    if x == 0 :
+        print("ok")
     else:
-        return 0
+        print("not ok")
 
 def health_check(ip_address):
     global index
-    command = ["ping","-c","4",ip_address]
+    command = ["ping",ip_address]
     ping_command = subprocess.run(command,capture_output=True,text=True)
     #exit_code = ping_command = subprocess.call(command)
     if ping_command.returncode == 0 :
         #print("the connection is OK !")
         #return exit_code
-        count+1 
+        pass
     else :
         #print("the connection is'nt Ok !")
         #return exit_code
         index+=1
 
-lst = ["8.8.8.8","192.168.1.8"]
+lst = ["8.8.8.8","192.168.64.1"]
 for i in lst:
     health_check(ip_address=i)
-a(index,count)
+a(index)
+
