27efe5cad4f3f0e1570b70d52db59d35690f455f
jenkins <amirmahdifhp@gmail.com>
2025-06-07 06:39:31 -0400

    Update ,update.sh

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+var=/etc/os-release
+
+if grep -q "Arch" $var
+then
+    # it will update for arch system
+    sudo pacman -Syu
+fi
+
+if grep -q "Ubuntu" $var || grep -q "Pop" $var
+then
+    # update on ubuntu and pop-os
+    sudo apt update
+    sudo apt upgrade
+fi
