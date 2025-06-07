20b02504f678d6acafee24aeb61cfe704c6c2a7a
linux-best <amirmahdifhp@gmail.com>
2025-06-07 18:22:26 +0330

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,7 +4,7 @@ backup_destination=$2
 backup_Display_name=$3
 
 # Backup_Command
-tar -cvpzf $backup_Display_name.tar.gz $backup_source && mv $backup_Display_name.tar.gz $backup_destination
+sudo tar -cvpzf $backup_Display_name.tar.gz $backup_source && sudo mv $backup_Display_name.tar.gz $backup_destination
 
 # Exit_check_code
 if [ $? -eq 0 ]
