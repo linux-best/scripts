332a73b73dde4272276d5fdd39be9024f0ee0460
jenkins <amirmahdifhp@gmail.com>
2025-06-07 11:37:15 -0400

    Update ,Health_check.py

--- Health_check.py
+++ Health_check.py
@@ -13,16 +13,10 @@ def health_check(ip_address):
     ping_command = subprocess.run(command,capture_output=True,text=True)
     #exit_code = ping_command = subprocess.call(command)
     if ping_command.returncode == 0 :
-        #print("the connection is OK !")
-        #return exit_code
         pass
-    else :
-        #print("the connection is'nt Ok !")
-        #return exit_code
+    else:
         index+=1
-
-lst = ["8.8.8.8","192.168.64.1"]
+lst = ["8.8.8.8","192.168.1.7"]
 for i in lst:
     health_check(ip_address=i)
 a(index)
-
