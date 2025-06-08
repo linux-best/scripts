f5d9fd4c8c16c0e8cfceda50bf8b1c16b511af68
linux-best <amirmahdifhp@gmail.com>
2025-06-08 16:29:30 +0330

    Update Health_check.py

--- Health_check.py
+++ Health_check.py
@@ -12,11 +12,11 @@ def health_check(ip_address):
     command = ["ping",ip_address,"-c","4"]
     ping_command = subprocess.run(command,capture_output=True,text=True)
     #exit_code = ping_command = subprocess.call(command)
-    if ping_command.returncode == 0 :
-        pass
-    else:
+    if ping_command.returncode != 0 :
         index+=1
+    
 lst = ["8.8.8.8","192.168.1.7"]
 for i in lst:
     health_check(ip_address=i)
 a(index)
+
